9d6abda8060bb87d6cf17003d4888d9cde083456
Ming-der Wang <mingderwang@gmail.com>
2021-04-12 00:21:56 +0800

    reset for ChainLink 2021 spring hackathon

---
+++ packages/hardhat/test/myTest.js
@@ -0,0 +1,26 @@
+const { ethers } = require("hardhat");
+const { use, expect } = require("chai");
+const { solidity } = require("ethereum-waffle");
+
+use(solidity);
+
+describe("My Dapp", function () {
+  let myContract;
+
+  describe("TokenFactory", function () {
+    it("Should deploy TokenFactory", async function () {
+      const TokenFactory = await ethers.getContractFactory("TokenFactory");
+
+      myContract = await TokenFactory.deploy();
+    });
+
+    describe("setPurpose()", function () {
+      it("Should be able to set a new purpose", async function () {
+        const newPurpose = "Test Purpose";
+
+        await myContract.setPurpose(newPurpose);
+        expect(await myContract.purpose()).to.equal(newPurpose);
+      });
+    });
+  });
+});
